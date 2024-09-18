03a6bfdd2bf858eb155817ad6cc5fafa6235ed61
jackdingman <143650685+jackdingman@users.noreply.github.com>
2024-09-18 11:25:02 -0500

    Initial Project Setup
    Created the project. Started on the Event class and added necessary methods and variables.

---
+++ src/Event.java
@@ -0,0 +1,23 @@
+import java.time.*;
+
+abstract class Event implements Comparable<Event> {
+
+    private String name;                            //name of the event
+    private LocalDateTime dateTime;                 //time and date that the event starts
+
+    //String methods are abstract because they will have unique implementations, where implementation of LocalDateTime should be the same.
+
+    public abstract String getName();                   //getter for name of the event
+    public LocalDateTime getDateTime(){                 //getter for dateTime
+        return dateTime;
+    }
+    public void setDateTime(LocalDateTime dateTime) {   //sets the dateTime
+    this.dateTime = dateTime;
+    }
+    public abstract void setName(String name);          //sets name of the event
+
+    @Override
+    public int compareTo(Event o) {
+        return 0;
+    }
+}
